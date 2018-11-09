1a75d981c6ae50e3790bc8f534fc07d342363562
rockan007 <rockan007@gmail.com>
2018-11-09 17:22:33 +0800

    表单提交，委托给doGet

--- src/main/java/com/lostbug/hello/HelloServlet.java
+++ src/main/java/com/lostbug/hello/HelloServlet.java
@@ -1,21 +1,49 @@
 package com.lostbug.hello;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.getWriter().println("Hello,World!");
+    private static final String DEFAULT_USER = "Guest";
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.getWriter().println("Hello,World!");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = req.getParameter("user");
+        if (user == null) {
+            user = HelloServlet.DEFAULT_USER;
+        }
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter writer = resp.getWriter();
+        writer.append("<!DOCTYPE html>\r\n")
+                .append("<html>\r\n")
+                .append("   <head>\r\n")
+                .append("       <title>Hello User Application</title>")
+                .append("   </head>")
+                .append("   <body>\r\n")
+                .append("       Hello, ").append(user).append("!<br/><br/>\r\n")
+                .append("       <form action=\"greeting\" method=\"POST\">\r\n")
+                .append("           Enter your name:<br/>\r\n")
+                .append("           <input type=\"text\" name=\"user\"/><br/>\r\n")
+                .append("           <input type=\"submit\" value=\"Submit\"/>\r\n")
+                .append("       </form>")
+                .append("   </body>\r\n")
+                .append("</html>\r\n");
+
     }
 
     @Override
     public void init() throws ServletException {
         super.init();
-        System.out.println("Servlet " + this.getServletName() + " has started");
     }
 
     @Override
